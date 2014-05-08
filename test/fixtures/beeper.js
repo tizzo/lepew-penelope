4b4de620900f150310b5bc0b50322b8fd7df95d7
Howard Tyson <howard@howardtyson.com>
2014-05-08 09:07:29 -0400

    Updating test script to take commands for what to run.

--- test/fixtures/beeper.js
+++ test/fixtures/beeper.js
@@ -1,3 +1,17 @@
-setInterval(function() {
-  console.log('beep');
+// A simple test script used to verify that we're getting the right output
+// from our process wrapper.
+
+var beep = setInterval(function() {
+  console.log(process.argv[4] || 'beep');
 }, process.argv[2]);
+
+var boop = setInterval(function() {
+  console.error(process.argv[5] || 'boop');
+}, process.argv[2]);
+
+setTimeout(function() {
+  clearInterval(beep);
+  clearInterval(boop);
+  console.log('here');
+  process.exit(1);
+}, process.argv[3]);
