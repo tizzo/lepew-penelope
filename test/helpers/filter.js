8bd2459d5edb7fb7d0e26b984bd23f1cfd9787cd
Howard Tyson <howard@howardtyson.com>
2014-05-10 14:56:11 -0400

    Document our filter function from our test helper.

--- test/helpers/filter.js
+++ test/helpers/filter.js
@@ -1,5 +1,8 @@
 var es = require('event-stream');
 
+// Creates a through stream that filters events based on
+// whether they match the simple criteria passed into the
+// options array.
 module.exports = function filter(options) {
   return es.through(function(data) {
     var option = null;
