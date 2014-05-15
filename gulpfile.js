f15a6e13772e95feefbd289020e7696d86680834
Howard Tyson <howard@howardtyson.com>
2014-05-15 09:51:44 -0400

    Updating gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var es = require('event-stream');
 
-gulp.task('default', function() {
-  gulp.src('test/testRunCommands.js')
-    .pipe(mocha({reporter: 'spec'}));
+var paths = {
+  tests: ['test/test*.js'],
+  src: ['index.js'],
+};
+
+gulp.task('test', function() {
+  try {
+    gulp.src(paths.tests)
+      .pipe(mocha({reporter: 'spec'}));
+  }
+  catch (e) {
+    console.error('Mocha tests failed');
+  }
 });
+
+gulp.task('jshint', function() {
+  var through = es.through();
+  gulp.src(paths.src)
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.tests, ['jshint', 'test']);
+  gulp.watch(paths.src, ['jshint', 'test']);
+});
+
+gulp.task('default', ['test']);
 
 
