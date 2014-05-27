6b7ca2f002324599b22feef0f4812d2128e3ae1e
Howard Tyson <howard@howardtyson.com>
2014-05-27 09:21:50 -0400

    Updating local stream references to differentiate stdout and stderr.

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ Penelope.prototype.runCommand = function(name, command, args, done) {
     .pipe(es.split())
     .pipe(this.createEventStream(name, command, 'stderr'))
     .pipe(this.eventStream);
-  this.processStreams[name] = child;
 };
 
 /**
@@ -87,7 +86,7 @@ Penelope.prototype.runCommand = function(name, command, args, done) {
  */
 Penelope.prototype.createEventStream = function(name, command, streamName) {
   var self = this;
-  return es.through(function(data) {
+  return this.processStreams[name + ':' + streamName] = es.through(function(data) {
     if (data === '') {
       return;
     }
@@ -101,7 +100,7 @@ Penelope.prototype.createEventStream = function(name, command, streamName) {
   },
   // Don't end our event stream until all of the child processes have exited.
   function() {
-    delete self.processStreams[name];
+    delete self.processStreams[name + ':' + streamName];
     if (Object.keys(self.processStreams).length === 0) {
       this.emit('end');
     }
