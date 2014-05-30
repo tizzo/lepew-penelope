a6a4603a1d0c1427ce12ec1b88a00c63f8afe39a
Howard Tyson <howard@howardtyson.com>
2014-05-30 00:20:04 -0400

    Add a config structure validation function.

---
+++ test/testValidateConfigStructure.js
@@ -0,0 +1,23 @@
+var should = require('should');
+var validator = require('../lib/validateConfigStructure');
+
+describe('validateConfigStructure', function() {
+  it('should that the command is present', function() {
+    validator({'args': []}).should.equal(false);
+  });
+  it('should not accept an array', function() {
+    validator([]).should.equal(false);
+  });
+  it('should ensure args is an array', function() {
+    validator({'command': 'foo', 'args': 'test'}).should.equal(false);
+    validator({'command': 'foo', 'args': {'foo': 'bar'}}).should.equal(false);
+  });
+  it('should ensure options is an object', function() {
+    validator({'command': 'foo', 'options': []}).should.equal(false);
+    validator({'command': 'foo', 'options': 'test'}).should.equal(false);
+    validator({'command': 'foo', 'options': {'foo': 'bar'}}).should.equal(true);
+  });
+  it('should not accept unrecognized attributes', function() {
+    validator({'command': 'foo', 'foo': 'bar'}).should.equal(false);
+  });
+});
